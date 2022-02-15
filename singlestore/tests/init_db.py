83149132ffb6ac73a566a1d3c5d80fd2c05dae43
Kevin Smith <Kevin.Smith@octobergray.com>
2022-02-15 12:22:44 -0600

    Remove HTTP port

--- singlestore/tests/init_db.py
+++ singlestore/tests/init_db.py
@@ -2,17 +2,9 @@
 # type: ignore
 from __future__ import annotations
 
-import os
-
 import singlestore as s2
 
 commands = r'''
-SET GLOBAL HTTP_PROXY_PORT={SINGLESTORE_HTTP_PORT};
-
-SET GLOBAL HTTP_API=ON;
-
-RESTART PROXY;
-
 DROP DATABASE IF EXISTS app;
 
 CREATE DATABASE app;
@@ -24,7 +16,7 @@ CREATE ROWSTORE TABLE IF NOT EXISTS data (
     value VARCHAR(255) NOT NULL,
     PRIMARY KEY (id) USING HASH
 ) DEFAULT CHARSET = utf8 COLLATE = utf8_unicode_ci;
-'''.format(SINGLESTORE_HTTP_PORT=os.environ.get('SINGLESTORE_HTTP_PORT', 9000))
+'''
 
 with s2.connect() as conn:
     with conn.cursor() as cur:
