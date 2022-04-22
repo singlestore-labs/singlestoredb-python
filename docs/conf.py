9ef4ae4fe3e25db65bf6392396ea7109208efa96
Kevin Smith <Kevin.Smith@octobergray.com>
2022-04-22 13:04:00 -0500

    Update doc style

--- docs/conf.py
+++ docs/conf.py
@@ -18,7 +18,7 @@ sys.path.insert(0, os.path.abspath('..'))
 # -- Project information -----------------------------------------------------
 
 project = 'SingleStore'
-copyright = '2022 SingleStore. All Rights Reserved.'
+copyright = '2022 SingleStore. All Rights Reserved'
 author = 'SingleStore'
 
 
