a53b090d1af35d525193b6b242f9a73da47ab9b8
Kevin Smith <Kevin.Smith@octobergray.com>
2022-10-24 09:06:38 -0500

    Add accelerator module in

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python
 """SingleStoreDB package installer."""
-# from setuptools import Extension
+from setuptools import Extension
 from setuptools import setup
 
 setup()
 
-# setup(
-#     ext_modules=[
-#         Extension(
-#             '_pymysqlsv',
-#             ['singlestoredb/clients/pymysqlsv/accel.c'],
-#             py_limited_api=True,
-#         ),
-#     ],
-# )
+setup(
+    ext_modules=[
+        Extension(
+            '_pymysqlsv',
+            ['singlestoredb/clients/pymysqlsv/accel.c'],
+            py_limited_api=True,
+        ),
+    ],
+)
