f743fc834c9643daf90b1c373a6bc74178631171
Kevin Smith <Kevin.Smith@octobergray.com>
2022-11-30 15:52:41 -0600

    More Windows tests

--- conda.recipe/bld.bat
+++ conda.recipe/bld.bat
@@ -1,5 +1,7 @@
 @echo on
 
 dir
+dir dist
 
-python -m pip install --no-deps --ignore-installed %SRC_DIR\%dist\singlestoredb-*win*.whl
+"%PYTHON%" -m pip install --no-deps --ignore-installed dist/singlestoredb-0.4.0-cp36-abi3-win_amd64.whl
+if errorlevel 1 exit 1
