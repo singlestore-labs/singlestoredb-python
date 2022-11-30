5a06f0576633370f6576eda5cd724aca66876116
Kevin Smith <Kevin.Smith@octobergray.com>
2022-11-30 16:04:27 -0600

    Use env var for package version

--- conda.recipe/bld.bat
+++ conda.recipe/bld.bat
@@ -3,5 +3,5 @@
 dir
 dir dist
 
-"%PYTHON%" -m pip install --no-deps --ignore-installed dist/singlestoredb-0.4.0-cp36-abi3-win_amd64.whl
+"%PYTHON%" -m pip install --no-deps --ignore-installed "dist/singlestoredb-%PKG_VERSION%-cp36-abi3-win_amd64.whl"
 if errorlevel 1 exit 1
