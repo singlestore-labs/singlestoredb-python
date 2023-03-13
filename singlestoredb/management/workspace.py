62e74e8619954a14d29de18bb1edbdca5dad8ca4
Adalbert Makarovych <amakarovych-ua@singlestore.com>
2023-03-13 16:02:53 +0200

    Added expiresAt argument to the create_workspace_group

--- singlestoredb/management/workspace.py
+++ singlestoredb/management/workspace.py
@@ -452,12 +452,12 @@ class WorkspaceManager(Manager):
             Admin password for the workspace group. If no password is supplied,
             a password will be generated and retured in the response.
         expires_at : str, optional
-            The timestamp of when the workspace group will expire. 
-            If the expiration time is not specified, the workspace group will have no expiration time. 
-            At expiration, the workspace group is terminated and all the data is lost. 
+            The timestamp of when the workspace group will expire.
+            If the expiration time is not specified, the workspace group will have no expiration time.
+            At expiration, the workspace group is terminated and all the data is lost.
             Expiration time can be specified as a timestamp or duration.
             Example: "2021-01-02T15:04:05Z07:00", "2021-01-02", "3h30m"
-            
+
         Returns
         -------
         :class:`WorkspaceGroup`
