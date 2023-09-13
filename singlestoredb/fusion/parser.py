54543aab55ea24fa95b88078aefa73ca3d6b66df
Kevin Smith <Kevin.Smith@octobergray.com>
2023-09-13 09:47:15 -0500

    Move grammar to separate file

--- singlestoredb/fusion/parser.py
+++ singlestoredb/fusion/parser.py
@@ -7,40 +7,10 @@ from typing import Iterable
 from typing import List
 from typing import Optional
 
-from parsimonious.grammar import Grammar
 from parsimonious.nodes import Node
 from parsimonious.nodes import NodeVisitor
 
-
-GRAMMAR = Grammar(r'''
-    init = ws ( show_users / show_regions / show_workspaces / show_workspace_groups / select / create_workspace_group / create_workspace ) ws end? ws
-    ws = ~"(\s*(/\*.*\*/)*\s*)*"
-    like = ~"like"i ws qs ws
-    extended = ~"extended"i ws
-    in = ~"in"i ws qs ws
-    qs = ~"\"[^\"]*\"|'[^\']*'" ws
-    ident = ~"(`[^`]`\.|\w+\.)?(`[^`]+`|\w+|\*)" ws
-    name = ~"[A-Z]\w*" ws
-    compound_name = ident alias? ws
-    comma = "," ws
-    end = ";" ws
-    alias = ~"as"i ws ~"(`[^`]+`|\w+)" ws
-    projections = compound_name alias? (comma compound_name alias?)* ws
-    table = compound_name ws
-    select = ~"select"i ws projections ~"from"i ws table ws
-    in_region = ~"in"i ws ~"region" ws (~"id" ws)? qs ws
-    with_password = ~"with"i ws ~"password" ws qs ws
-    with_firewall_ranges = ~"with"i ws ~"firewall"i ws ~"ranges"i ws qs (comma qs)* ws
-    expires_at = ~"expires"i ws ~"at" ws qs ws
-    in_group = ~"in"i ws ~"group" ws (~"id" ws)? qs ws
-    with_size = ~"with"i ws ~"size"i ws qs ws
-    show_users = ~"show"i ws ~"users"i ws like? ws
-    show_workspaces = ~"show"i ws ~"workspaces"i ws in_group ( like / extended )* ws
-    show_workspace_groups = ~"show"i ws ~"workspace"i ws ~"groups"i ws ( like / extended )* ws
-    show_regions = ~"show"i ws ~"regions"i ws like? ws
-    create_workspace_group = ~"create"i ws ~"workspace"i ws ~"group" ws qs in_region ( with_password / expires_at / with_firewall_ranges )* ws
-    create_workspace = ~"create"i ws ~"workspace"i ws qs in_group with_size ws
-''')
+from .grammar import GRAMMAR
 
 
 def flatten(items: Iterable[Any]) -> List[Any]:
@@ -228,7 +198,11 @@ class SQLParser(NodeVisitor):
     def visit_expires_at(self, node: Node, visited_children: Iterable[Any]) -> Any:
         return Modifier('expires_at', flatten(visited_children)[-1])
 
-    def visit_with_firewall_ranges(self, node: Node, visited_children: Iterable[Any]) -> Any:
+    def visit_with_firewall_ranges(
+        self,
+        node: Node,
+        visited_children: Iterable[Any],
+    ) -> Any:
         _, _, _, *ranges = flatten(visited_children)
         return Modifier('with_firewall_ranges', ranges)
 
