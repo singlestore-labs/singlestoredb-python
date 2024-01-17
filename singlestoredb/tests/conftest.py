c4a88a04c0f4a4376a782bebbda5588e86b6ee2f
Robin <me@esoterra.dev>
2024-01-17 15:32:53 -0500

    fix(pytest): Rename entrypoint and remove conftest.py

--- singlestoredb/tests/conftest.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-"""Configure Pytest"""
-# Load the pytest plugin so that the fixtures are available
-from singlestoredb import pytest
-
-# Module is not directly used
-del pytest
