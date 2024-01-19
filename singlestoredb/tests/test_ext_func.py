39d2aaf5d7f928581ec021e44bc991ef8e89b46a
Kevin Smith <Kevin.Smith@octobergray.com>
2024-01-19 15:48:11 -0600

    Add error to HTTP

--- singlestoredb/tests/test_ext_func.py
+++ singlestoredb/tests/test_ext_func.py
@@ -219,12 +219,6 @@ class TestExtFunc(unittest.TestCase):
         assert desc[0].type_code == ft.DOUBLE
         assert desc[0].null_ok is False
 
-        if 'http' in os.environ.get('SINGLESTOREDB_URL', ''):
-            self.cur.execute(
-                'select arrow_double_mult(value, NULL) as res '
-                'from data order by id',
-            )
-
         # NULL is not valid
         with self.assertRaises(self.conn.OperationalError):
             self.cur.execute(
