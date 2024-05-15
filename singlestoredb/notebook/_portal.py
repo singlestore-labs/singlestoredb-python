66be125b9745854ba5834d50cf598a4d981be253
Kevin Smith <Kevin.Smith@octobergray.com>
2024-05-15 12:41:52 -0500

    Update event / command syntax

--- singlestoredb/notebook/_portal.py
+++ singlestoredb/notebook/_portal.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python
 import json
 import os
+import re
+import time
 import urllib.parse
 from typing import Any
+from typing import Callable
 from typing import Dict
 from typing import Optional
 
-from ..management import workspace as _ws
+from . import _objects as obj
+from ..management import workspace as mgr
 
 try:
     from IPython import get_ipython
@@ -21,6 +25,7 @@ class Portal(object):
 
     def __init__(self) -> None:
         self._connection_info: Dict[str, Any] = {}
+        self._theme_info: Dict[str, Any] = {}
 
         if has_ipython:
             try:
@@ -31,99 +36,175 @@ class Portal(object):
 
     def __str__(self) -> str:
         attrs = []
-        for name in sorted(dir(type(self))):
-            if name.startswith('_'):
-                continue
-            if isinstance(getattr(type(self), name), property):
-                if name == 'password':
-                    if self.password is not None:
-                        attrs.append("password='***'")
-                    else:
-                        attrs.append('password=None')
+        for name in [
+            'organization_id', 'workspace_group_id', 'workspace_id',
+            'host', 'port', 'user', 'password', 'default_database',
+        ]:
+            if name == 'password':
+                if self.password is not None:
+                    attrs.append("password='***'")
                 else:
-                    attrs.append(f'{name}={getattr(self, name)!r}')
+                    attrs.append('password=None')
+            else:
+                attrs.append(f'{name}={getattr(self, name)!r}')
         return f'{type(self).__name__}({", ".join(attrs)})'
 
     def __repr__(self) -> str:
         return str(self)
 
-    def _post_message(self, command: str, data: Dict[str, Any]) -> None:
-        if not has_ipython:
+    def _post_message(
+        self,
+        command: str,
+        data: Dict[str, Any],
+        wait_on_condition: Optional[Callable[[], bool]] = None,
+        timeout_message: str = 'timed out waiting for message',
+        wait_interval: float = 0.2,
+        timeout: float = 5.0,
+    ) -> None:
+        if not has_ipython or not data:
             return
-        json_data = json.dumps(data).replace("'", "\\'")
-        display.display(
-            display.Javascript(f'''
-            window.postMessage(
-                '{{"command": "singlestore.portal.{command}", "data": {json_data}}}',
-                window.location.origin
-            );
-        '''),
+
+        msg = repr(
+            json.dumps(
+                dict(
+                    type='command',
+                    name=f'singlestore.portal.{command}',
+                    data=data,
+                ),
+            ),
         )
 
+        display.display(
+            display.Javascript(f'window.postMessage({msg}, window.location.origin);'),
+        )
+
+        if wait_on_condition is not None:
+            elapsed = 0.0
+            while True:
+                if wait_on_condition():
+                    break
+                if elapsed > timeout:
+                    raise RuntimeError(timeout_message)
+                time.sleep(wait_interval)
+                elapsed += wait_interval
+
     def _request(self, stream: Any, ident: Any, msg: Dict[str, Any]) -> None:
+        """Handle request on the control stream."""
         if not isinstance(msg, dict):
             return
-
         content = msg.get('content', {})
-        kind = content.get('kind', 'unknown')
+        if content.get('type', '') != 'command':
+            return
+        func = getattr(self, '_handle_' + content.get('name', 'unknown').split('.')[-1])
+        if func is not None:
+            func(content.get('data', {}))
 
-        if kind == 'UPDATE_CONNECTION':
-            self._connection_info.update(content)
+    def _handle_connection_updated(self, data: Dict[str, Any]) -> None:
+        """Handle connection_updated event."""
+        self._connection_info.update(data)
 
-        elif kind == 'UPDATE_THEME':
-            pass
+    def _handle_theme_updated(self, data: Dict[str, Any]) -> None:
+        """Handle theme_updated event."""
+        self._theme_info.update(data)
 
-        elif kind == 'UPDATE_SETTINGS':
-            pass
+    def _handle_unknown(self, data: Dict[str, Any]) -> None:
+        """Handle unknown events."""
+        pass
 
     @property
     def organization_id(self) -> Optional[str]:
         """Organization ID."""
         try:
-            return self._connection_info['orgID']
+            return self._connection_info['organization']
         except KeyError:
             return os.environ.get('SINGLESTOREDB_ORGANIZATION')
+
+    @property
+    def organization(self) -> obj.Organization:
+        """Organization."""
+        return obj.organization
+
+    @property
+    def theme(self) -> Optional[str]:
+        """Theme."""
+        return self._theme_info.get('theme')
+
+    @theme.setter
+    def theme(self, name: str) -> None:
+        """Set theme."""
+        self._post_message(
+            'update_theme', dict(theme=name),
+            wait_on_condition=lambda x: self.theme == name,  # type: ignore
+            timeout_message='timeout waiting for theme update',
+        )
+
+    @property
+    def stage(self) -> obj.Stage:
+        """Stage."""
+        return obj.stage
+
+    @property
+    def secrets(self) -> obj.Secrets:
+        """Secrets."""
+        return obj.secrets
 
     @property
     def workspace_group_id(self) -> Optional[str]:
         """Workspace Group ID."""
         try:
-            return self._connection_info['workspaceGroupID']
+            return self._connection_info['workspace_group']
         except KeyError:
             return os.environ.get('SINGLESTOREDB_WORKSPACE_GROUP')
+
+    @property
+    def workspace_group(self) -> obj.WorkspaceGroup:
+        """Workspace group."""
+        return obj.workspace_group
+
+    @workspace_group.setter
+    def workspace_group(self) -> None:
+        """Set workspace group."""
+        raise AttributeError(
+            'workspace group can not be set explictly; ' +
+            'you can only set a workspace',
+        )
 
     @property
     def workspace_id(self) -> Optional[str]:
         """Workspace ID."""
         try:
-            return self._connection_info['workspaceID']
+            return self._connection_info['workspace']
         except KeyError:
             return os.environ.get('SINGLESTOREDB_WORKSPACE')
 
     @property
-    def workspace(self) -> Optional[_ws.Workspace]:
-        return _ws.get_workspace(self.workspace_id)
+    def workspace(self) -> obj.Workspace:
+        """Workspace."""
+        return obj.workspace
 
     @workspace.setter
     def workspace(self, name_or_id: str) -> None:
-        w = _ws.get_workspace_group(self.workspace_group_id).workspaces[name_or_id]
-        self._post_message('update_connection', dict(workspace=w.id))
+        """Set workspace."""
+        if re.match(
+            r'[a-z0-9]{8}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{12}',
+            name_or_id, flags=re.I,
+        ):
+            id = name_or_id
+        else:
+            w = mgr.get_workspace_group(self.workspace_group_id).workspaces[name_or_id]
+            id = w.id
 
-        import time
-        i = 40
-        while True:
-            if self.workspace_id == w.id:
-                if i < 0:
-                    raise RuntimeError('timeout waiting for workspace change')
-                break
-            time.sleep(0.1)
-            i -= 1
+        self._post_message(
+            'update_connection', dict(workspace=w.id),
+            wait_on_condition=lambda x: self.workspace_id == id,  # type: ignore
+            timeout_message='timeout waiting for workspace update',
+        )
 
     @property
     def cluster_id(self) -> Optional[str]:
         """Cluster ID."""
         try:
-            return self._connection_info['clusterID']
+            return self._connection_info['cluster']
         except KeyError:
             return os.environ.get('SINGLESTOREDB_CLUSTER')
 
@@ -136,8 +217,32 @@ class Portal(object):
             port=url.port or None,
             user=url.username or None,
             password=url.password or None,
-            defaultDatabase=url.path.split('/')[-1] or None,
+            default_database=url.path.split('/')[-1] or None,
         )
+
+    @property
+    def connection_url(self) -> Optional[str]:
+        """Connection URL."""
+        if self._connection_info:
+            info = self._connection_info
+            host = urllib.parse.quote_plus(info.get('host', ''))
+            port = urllib.parse.quote_plus(info.get('port', ''))
+            user = urllib.parse.quote_plus(info.get('user', ''))
+            password = urllib.parse.quote_plus(info.get('password', ''))
+            db = urllib.parse.quote_plus(info.get('default_database', ''))
+
+            scheme = os.environ.get('SINGLESTOREDB_URL', 'https').split(':')[0]
+            creds = f'{user}:{password}@' if user else ''
+            netloc = f'{host}:{port}' if port else host
+
+            return f'{scheme}://{creds}{netloc}/{db}'
+
+        return os.environ.get('SINGLESTOREDB_URL', None)
+
+    @property
+    def mongo_url(self) -> Optional[str]:
+        """Mongo URL."""
+        return self._connection_info.get('kai_url')
 
     @property
     def host(self) -> Optional[str]:
@@ -175,24 +280,18 @@ class Portal(object):
     def default_database(self) -> Optional[str]:
         """Default database."""
         try:
-            return self._connection_info['defaultDatabase']
+            return self._connection_info['default_database']
         except KeyError:
-            return self._parse_url()['defaultDatabase']
+            return self._parse_url()['default_database']
 
     @default_database.setter
     def default_database(self, name: str) -> None:
         """Set default database."""
-        self._post_message('update_connection', dict(database=name))
-
-        import time
-        i = 40
-        while True:
-            if self.default_database == name:
-                if i < 0:
-                    raise RuntimeError('timeout waiting for database change')
-                break
-            time.sleep(0.1)
-            i -= 1
+        self._post_message(
+            'update_connection', dict(database=name),
+            wait_on_condition=lambda x: self.default_database == name,  # type: ignore
+            timeout_message='timeout waiting for database update',
+        )
 
     @property
     def version(self) -> Optional[str]:
