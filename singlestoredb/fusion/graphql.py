1082197d6c3165355f0fb7a76b4a75c57a2b80c8
Ema Vieira <evieira@singlestore.com>
2024-06-14 15:09:25 +0100

    remove https

--- singlestoredb/fusion/graphql.py
+++ singlestoredb/fusion/graphql.py
@@ -14,7 +14,7 @@ from . import result
 from .result import FusionSQLResult
 
 
-API_URL = 'https://localhost:9001/public'
+API_URL = 'http://localhost:9001/public'
 
 
 def pass_through(x: Any) -> Any:
