e006024f8ce7627dbc6b2196c280f10dbb5d1677
Kevin D Smith <kevin.smith@octobergray.com>
2024-08-20 09:34:31 -0500

    Update test_connection.py

--- singlestoredb/tests/test_connection.py
+++ singlestoredb/tests/test_connection.py
@@ -2780,7 +2780,8 @@ class TestConnection(unittest.TestCase):
         if self.conn.driver not in ['http', 'https']:
             with s2.connect(database=type(self).dbname, client_found_rows=False) as conn:
                 with conn.cursor() as cur:
-                    table_name = f'test_client_found_rows_{uuid.uuid4()}'.replace('-', '_')
+                    tag = uuid.uuid4().replace('-', '_')
+                    table_name = f'test_client_found_rows_{tag}'
                     cur.execute(f"CREATE TABLE {table_name} (id BIGINT \
                                 PRIMARY KEY, s TEXT DEFAULT 'def');")
                     cur.execute(f'INSERT INTO {table_name} (id) \
