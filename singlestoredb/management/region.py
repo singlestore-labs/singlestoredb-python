2faba79b87102555a67aaf03f0b45b23505760d1
Kevin Smith <Kevin.Smith@octobergray.com>
2024-10-11 14:54:19 -0500

    Add region codes

--- singlestoredb/management/region.py
+++ singlestoredb/management/region.py
@@ -7,6 +7,44 @@ from .manager import Manager
 from .utils import vars_to_str
 
 
+CODES = {
+    'AWS': {
+        'US East (N. Virginia)': 'us-east-1',
+        'US East (Ohio)': 'us-east-2',
+        'US West (N. California)': 'us-west-1',
+        'US West (Oregon)': 'us-west-2',
+        'Africa (Cape Town)': 'af-south-1',
+        'Asia Pacific (Hong Kong)': 'ap-east-1',
+        'Asia Pacific (Hyderabad)': 'ap-south-2',
+        'Asia Pacific (Jakarta)': 'ap-southeast-3',
+        'Asia Pacific (Malaysia)': 'ap-southeast-5',
+        'Asia Pacific (Melbourne)': 'ap-southeast-4',
+        'Asia Pacific (Mumbai)': 'ap-south-1',
+        'Asia Pacific (Osaka)': 'ap-northeast-3',
+        'Asia Pacific (Seoul)': 'ap-northeast-2',
+        'Asia Pacific (Singapore)': 'ap-southeast-1',
+        'Asia Pacific (Sydney)': 'ap-southeast-2',
+        'Asia Pacific (Tokyo)': 'ap-northeast-1',
+        'Canada (Central)': 'ca-central-1',
+        'Canada West (Calgary)': 'ca-west-1',
+        'China (Beijing)': 'cn-north-1',
+        'China (Ningxia)': 'cn-northweast-1',
+        'Europe (Frankfurt)': 'eu-central-1',
+        'Europe (Ireland)': 'eu-west-1',
+        'Europe (London)': 'eu-west-2',
+        'Europe (Milan)': 'eu-south-1',
+        'Europe (Paris)': 'eu-west-3',
+        'Europe (Spain)': 'eu-south-2',
+        'Europe (Stockholm)': 'eu-north-1',
+        'Europe (Zurich)': 'eu-central-2',
+        'Israel (Tel Aviv)': 'il-central-1',
+        'Middle East (Bahrain)': 'me-south-1',
+        'Middle East (UAE)': 'me-central-1',
+        'South America (São Paulo)': 'sa-east-1',
+    },
+}
+
+
 class Region(object):
     """
     Cluster region information.
@@ -65,3 +103,7 @@ class Region(object):
         )
         out._manager = manager
         return out
+
+    @property
+    def code(self) -> str:
+        return CODES[self.provider][self.name]
