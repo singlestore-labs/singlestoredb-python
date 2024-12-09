5b1fe5176b59c9aa250b866b630f44459ecd9aff
Nuno Gonçalves <ngoncalves@singlestore.com>
2024-12-09 20:10:11 +0000

    test

--- singlestoredb/magics/run_personal.py
+++ singlestoredb/magics/run_personal.py
@@ -1,5 +1,5 @@
 import os
-import time
+import tempfile
 from typing import Any
 
 from IPython.core.interactiveshell import InteractiveShell
@@ -32,6 +32,7 @@ class RunPersonalMagic(Magics):
           %run_personal {{ sample_notebook_name }}
 
         """
+
         template = Template(line.strip())
         personal_file = template.render(local_ns)
         if not personal_file:
@@ -42,16 +43,14 @@ class RunPersonalMagic(Magics):
             if not personal_file:
                 raise ValueError('No personal file specified.')
 
-        local_filename = (
-            f'{int(time.time() * 1_000_000)}_{personal_file}'.replace(' ', '_')
-        )
-        sql_command = f"DOWNLOAD PERSONAL FILE '{personal_file}' TO '{local_filename}'"
+        with tempfile.TemporaryDirectory() as temp_dir:
+            temp_file_path = os.path.join(temp_dir, personal_file)
+            sql_command = (
+                f"DOWNLOAD PERSONAL FILE '{personal_file}' "
+                f"TO '{temp_file_path}'"
+            )
 
-        # Execute the SQL command
-        self.shell.run_line_magic('sql', sql_command)
-        # Run the downloaded file
-        self.shell.run_line_magic('run', local_filename)
-
-        # Delete the local file after running it
-        if os.path.exists(local_filename):
-            os.remove(local_filename)
+            # Execute the SQL command
+            self.shell.run_line_magic('sql', sql_command)
+            # Run the downloaded file
+            self.shell.run_line_magic('run', f'"{temp_file_path}"')
