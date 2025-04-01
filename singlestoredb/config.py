cfdbffc5eb9f7676cd7f731fa827f39379596e9e
Kevin Smith <Kevin.Smith@octobergray.com>
2025-04-01 09:51:50 -0500

    Change default charset to utf8mb4

--- singlestoredb/config.py
+++ singlestoredb/config.py
@@ -80,7 +80,7 @@ register_option(
 )
 
 register_option(
-    'charset', 'string', check_str, 'utf8',
+    'charset', 'string', check_str, 'utf8mb4',
     'Specifies the character set for the session.',
     environ='SINGLESTOREDB_CHARSET',
 )
