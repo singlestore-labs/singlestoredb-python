9055445c8a464aa10cac734e68e8d9f0dfc4918a
Michael Giannakopoulos <105240719+mgiannakopoulos@users.noreply.github.com>
2025-09-22 15:23:16 -0400

    feat: Introduce SingleStoreChatFactory and SingleStoreEmbeddingsFactory factories that instantiate various flavors of chat and embedding instances. (#94)
    * feat: Introduce SingleStoreChat wrapper that uses interchangeably OpenAI or AmazonBedrockConverse protocol.
    
    * Provide option for getting 'X-S2-OBO' token for every request.
    
    * Provide headers that indicate passthrough Amazon bedrock requests.
    
    * Hardcode dummy credentials and region info for ChatBedrockConverse client.
    
    * Rename 'region' parameter to 'region_name'.
    
    * Expose 'streaming' parameter setting its oposite value to 'disable_streaming' ChatBedrockConverse client.
    
    * Set the default value for 'streaming' paramter to True.
    
    * Remove the cache option.
    
    * Remove unsupported kargs from Bedrock calls.
    
    * Replace composition wrapper with a factory method.
    
    * Pass bedrock runtime client as client parameter.
    
    * Pass also the 'X-BEDROCK-CONVERSE' headers that indicate that the requets should be handled as passthrough from UMG.
    
    * Remove some amazon specific headers, along with validation, remove X-BEDROCK headers as well.
    
    * Remove commented out code; set max retries to 1 for Amazon Bedrock models.
    
    * Use 'Union' return type to satisfy pre-commit checks for python version 3.9.
    
    * Expose also the hostingPlatform for InferenceAPIInfo.
    
    * Do not use model prefix, rely on hosting platform.
    
    * Introduce SingleStoreEmbeddingsFactory; small fixes.
    
    * Fix openai langchain library.
    
    * Remove any comments that expose internal implementation details.
    
    * Minor fixes.

--- singlestoredb/ai/__init__.py
+++ singlestoredb/ai/__init__.py
@@ -1,2 +1,5 @@
+from .chat import SingleStoreChat  # noqa: F401
+from .chat import SingleStoreChatFactory  # noqa: F401
 from .chat import SingleStoreChatOpenAI  # noqa: F401
 from .embeddings import SingleStoreEmbeddings  # noqa: F401
+from .embeddings import SingleStoreEmbeddingsFactory  # noqa: F401
