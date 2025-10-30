15de5d0f5e1bf7d0e43daff26ca01a450fd7f82e
Mohit Verma <mverma@singlestore.com>
2025-10-30 11:09:34 +0530

    checks changes

--- singlestoredb/fusion/handlers/models.py
+++ singlestoredb/fusion/handlers/models.py
@@ -4,9 +4,9 @@ from typing import Any
 from typing import Dict
 from typing import Optional
 
+from .. import result
 from ..handler import SQLHandler
 from ..result import FusionSQLResult
-from .. import result
 from .files import ShowFilesHandler
 from .utils import get_file_space
 from .utils import get_inference_api
