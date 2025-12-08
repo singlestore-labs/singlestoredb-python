2b84f7b419d879445e5c6e1237d20daa6a088263
Kevin Smith <Kevin.Smith@octobergray.com>
2025-12-08 14:36:40 -0600

    Fix pre-commit issues

--- singlestoredb/functions/ext/asgi.py
+++ singlestoredb/functions/ext/asgi.py
@@ -50,13 +50,12 @@ import zipfile
 import zipimport
 from collections.abc import Awaitable
 from collections.abc import Iterable
+from collections.abc import Iterator
 from collections.abc import Sequence
 from types import ModuleType
 from typing import Any
 from typing import Callable
 from typing import Dict
-from typing import Iterable
-from typing import Iterator
 from typing import List
 from typing import Optional
 from typing import Set
@@ -93,6 +92,7 @@ except ImportError:
 
 
 logger = utils.get_logger('singlestoredb.functions.ext.asgi')
+
 
 async def to_thread(
     func: Any, /, *args: Any, **kwargs: Dict[str, Any],
