de14a96ce9a8d0cbe47c98738c6161c332b7a975
Kevin Smith <Kevin.Smith@octobergray.com>
2025-12-08 15:38:42 -0600

    Update imports

--- singlestoredb/functions/typing/numpy.py
+++ singlestoredb/functions/typing/numpy.py
@@ -6,26 +6,6 @@ import numpy as np
 import numpy.typing as npt
 from numpy import array  # noqa: F401
 
-NDArray = npt.NDArray
-
-StringArray = StrArray = npt.NDArray[np.str_]
-BytesArray = npt.NDArray[np.bytes_]
-Float16Array = HalfArray = npt.NDArray[np.float16]
-Float32Array = FloatArray = npt.NDArray[np.float32]
-Float64Array = DoubleArray = npt.NDArray[np.float64]
-IntArray = npt.NDArray[np.int_]
-Int8Array = npt.NDArray[np.int8]
-Int16Array = npt.NDArray[np.int16]
-Int32Array = npt.NDArray[np.int32]
-Int64Array = npt.NDArray[np.int64]
-UInt8Array = npt.NDArray[np.uint8]
-UInt16Array = npt.NDArray[np.uint16]
-UInt32Array = npt.NDArray[np.uint32]
-UInt64Array = npt.NDArray[np.uint64]
-DateTimeArray = npt.NDArray[np.datetime64]
-TimeDeltaArray = npt.NDArray[np.timedelta64]
-JSONArray = npt.NDArray[np.object_]
-=======
 try:
     from typing import TypeAlias  # type: ignore
 except ImportError:
@@ -47,6 +27,11 @@ StrArray: TypeAlias = StringArray
 BytesArray: TypeAlias = Annotated[
     npt.NDArray[np.bytes_], UDFAttrs(sql_type=sql_types.BLOB(nullable=False)),
 ]
+
+Float16Array: TypeAlias = Annotated[
+    npt.NDArray[np.float16], UDFAttrs(sql_type=sql_types.FLOAT16(nullable=False)),
+]
+HalfArray: TypeAlias = Float16Array
 
 Float32Array: TypeAlias = Annotated[
     npt.NDArray[np.float32], UDFAttrs(sql_type=sql_types.FLOAT(nullable=False)),
@@ -128,4 +113,3 @@ JSONArray: TypeAlias = Annotated[
 
 
 __all__ = ['array'] + [x for x in globals().keys() if x.endswith('Array')]
->>>>>>> fd486846 (Implement transformers for UDF inputs and outputs)
